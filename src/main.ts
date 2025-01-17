0ad72f534e6b94b6d0df6570772a333e4551bf20
Fathur0146 <105841113322@student.unismuh.ac.id>
2025-01-17 11:27:18 +0800

    done

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,6 @@ import { DocumentBuilder, SwaggerModule  } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser'
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   const config = new DocumentBuilder()
-//   .setTitle('Lab Backend')
-//   .setDescription('Fathur - 105841113322')
-//   .setVersion('1.0')
-//   .addTag('BelajarBackend')
-//   .build();
-// const documentFactory = () => SwaggerModule.createDocument(app, config);
-
-// SwaggerModule.setup('api', app, documentFactory);
-
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-// bootstrap();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
@@ -43,14 +27,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  // Aktifkan CORS
-  app.enableCors({
-    origin: 'http://localhost:5500', // URL frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
